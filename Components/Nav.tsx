fefcf63cd42038aaf04be095de0174789ffe354d
YoussefMassoud <youssefbusiness133@gmail.com>
2024-03-10 15:48:04 +0200

    add logo in nav

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -1,11 +1,17 @@
-    "use client";
+"use client";
+import Link from "next/link";
 
-    export default function Nav() {
-    return (
-        <div className="flex justify-center text-white space-x-5 py-4 ">
-            <div>Home</div>
-            <div>Activity</div>
-            <div>Terms of Service</div>
+export default function Nav() {
+  return (
+    <div className="mx-auto flex items-center px-24 py-4">
+      <h1 className="font-bold text-white text-2xl ">LOGO</h1>
+      <div className="grow">
+        <div className="flex justify-center items-center space-x-5 text-white">
+          <div>Home</div>
+          <div>Activity</div>
+          <div>Terms of Service</div>
         </div>
-    );
-    }
+      </div>
+    </div>
+  );
+}
