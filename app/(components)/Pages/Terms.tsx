b64cf1fd0b7a0b35ce5102abd867eb40f9a6d651
YoussefMassoud <youssefbusiness133@gmail.com>
2024-03-11 17:49:35 +0200

    make a terms of services page

--- app/(components)/Pages/Terms.tsx
+++ app/(components)/Pages/Terms.tsx
@@ -1,7 +1,76 @@
 import React from "react";
 
 const Terms = () => {
-  return <div>Terms of service page</div>;
+  return (
+    <section className="content-wrapper page more-page-active z-10  mt-6 ">
+      <div className="more-page flex flex-col row-gap-10 p-10 md:p-10">
+        <div className="more-page__device-select"></div>
+        <div className="more-page__terms-wrapper">
+          <br />
+          <div className="text-2xl text-[#FF3CF7]">
+            <ul className="list-decimal pl-8">
+              <li className="mb-2">
+                <strong>ABOUT KPG+ GAMES</strong>{" "}
+                <p className="text-gray-400 text-sm">
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                  Nulla non arcu lacinia neque faucibus fringilla. Nulla est.
+                  Fusce tellus odio, dapibus id fermentum quis, suscipit id
+                  erat. Nullam eget nisl. Integer imperdiet lectus quis justo.
+                  Duis bibendum, lectus ut viverra rhoncus,Lorem ipsum dolor sit
+                  amet, consectetuer adipiscing elit. Nulla non arcu lacinia
+                  neque faucibus fringilla. Nulla est. Fusce tellus odio,
+                  dapibus id fermentum quis, suscipit id erat. Nullam eget nisl.
+                  Integer imperdiet lectus quis justo. Duis bibendum, lectus ut
+                  viverra rhoncus,Lorem ipsum dolor sit amet, consectetuer
+                  adipiscing elit. Nulla non arcu lacinia neque faucibus
+                  fringilla. Nulla est. Fusce tellus odio, dapibus id fermentum
+                  quis, suscipit id erat. Nullam eget nisl. Integer imperdiet
+                  lectus quis justo. Duis bibendum, lectus ut viverra
+                  rhoncus,Lorem ipsum dolor sit amet, consectetuer adipiscing
+                  elit. Nulla non arcu lacinia neque faucibus fringilla. Nulla
+                  est. Fusce tellus odio, dapibus id fermentum quis, suscipit id
+                  erat. Nullam eget nisl. Integer imperdiet lectus quis justo.
+                  Duis bibendum, lectus ut viverra rhoncus,
+                </p>
+              </li>
+
+              <li>
+                <strong> Our Mission </strong>
+                <p className="text-gray-400 text-sm">
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                  Nulla non arcu lacinia neque faucibus fringilla. Nulla est.
+                  Fusce tellus odio, dapibus id fermentum quis, suscipit id
+                  erat. Nullam eget nisl. Integer imperdiet lectus quis justo.
+                  Duis bibendum, lectus ut viverra rhoncus,Lorem ipsum dolor sit
+                  amet, consectetuer adipiscing elit. Nulla non arcu lacinia
+                  neque faucibus fringilla. Nulla est. Fusce tellus odio,
+                  dapibus id fermentum quis, suscipit id erat. Nullam eget nisl.
+                  Integer imperdiet lectus quis justo. Duis bibendum, lectus ut
+                  viverra rhoncus,Lorem ipsum dolor sit amet, consectetuer
+                  adipiscing elit. Nulla non arcu lacinia neque faucibus
+                  fringilla. Nulla est. Fusce tellus odio, dapibus id fermentum
+                  quis, suscipit id erat. Nullam eget nisl. Integer imperdiet
+                  lectus quis justo. Duis bibendum, lectus ut viverra
+                  rhoncus,Lorem ipsum dolor sit amet, consectetuer adipiscing
+                  elit. Nulla non arcu lacinia neque faucibus fringilla. Nulla
+                  est. Fusce tellus odio, dapibus id fermentum quis, suscipit id
+                  erat. Nullam eget nisl. Integer imperdiet lectus quis justo.
+                  Duis bibendum, lectus ut viverra rhoncus,
+                </p>
+              </li>
+              {/* Add other ordered list items */}
+            </ul>
+            <br />
+            By accessing and using offerwall, you acknowledge that you have
+            read, understood, and agreed to abide by these terms. If you have
+            any questions or concerns, our support team is available to assist
+            you. Enjoy engaging with the offers and earning your well-deserved
+            rewards!
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Terms;
