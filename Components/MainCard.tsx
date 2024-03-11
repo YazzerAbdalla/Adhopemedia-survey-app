e392b0a598c25bf40375ee55fc2ebcf5bff0afbc
YoussefMassoud <youssefbusiness133@gmail.com>
2024-03-11 14:25:36 +0200

    main card done full

--- Components/MainCard.tsx
+++ Components/MainCard.tsx
@@ -6,8 +6,8 @@ export default function MainCard() {
     <div className="fixed left-0 right-0 top-56 flex justify-center items-center">
       <div className="max-w-sm rounded overflow-hidden border-2 border-[#4D229E]">
         <img
-          className="mx-auto"
-          src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg"
+          className="w-full max-h-48 min-h-48 "
+          src="https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/0a/b4/6c/0ab46cd6-d503-4c89-982a-3b8cb0750279/AppIcon-0-0-1x_U007emarketing-0-0-0-5-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/512x512bb.jpg"
           alt="Sunset in the mountains"
         />
         <div className="px-6 py-4 bg-black">
@@ -16,8 +16,14 @@ export default function MainCard() {
           </div>
           <p className="text-white text-sm">
             <span className="text-[#8D62DE]">Description :</span> Lorem ipsum
-            dolor sit amet
+            dolor sit amet 
           </p>
+          <div className="flex justify-start py-4">
+            <button
+              className=" border-2  border-[#8D62DE] rounded-3xl py-1 px-16 ">
+              💻
+            </button>
+          </div>
           <div className="flex justify-end py-4  ">
             <button
               className="text-[#180934] font-bold text-md  py-1 px-16"
@@ -25,7 +31,7 @@ export default function MainCard() {
                 background: "linear-gradient(92.16deg, #7655FE 0%, #EE77FF 37.1%, #FE6FFD 66.51%, #FF13DE 100%)",
               }}
             >
-             Reward
+             Reward 💰
             </button>
           </div>
         </div>
