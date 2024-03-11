4941aea87a204f5ad57b7ad5d5addcd070bcec73
YoussefMassoud <youssefbusiness133@gmail.com>
2024-03-11 20:38:24 +0200

    make y space in nav

--- app/(components)/Nav.tsx
+++ app/(components)/Nav.tsx
@@ -35,7 +35,7 @@ export default function Nav({ navTabs, setNavTabs }: navProps) {
         } lg:flex lg:items-center lg:w-auto`}
       >
         <div
-          className={`flex flex-col lg:py-0 lg:px-0 font-bold text-lg lg:font-normal lg:text-base py-5  px-4 lg:flex-row  lg:space-x-5 mt-3 w-full lg:justify-center text-white lg:bg-transparent`}
+          className={`flex flex-col lg:py-0 lg:space-y-0 space-y-5 lg:px-0 font-bold text-lg lg:font-normal lg:text-base py-5  px-4 lg:flex-row  lg:space-x-5 mt-3 w-full lg:justify-center text-white lg:bg-transparent`}
           style={{
             background:
               "linear-gradient(110.43deg, #180437 7.89%, #450B9D 89.43%)",
