825c553f7a30a546a110732e087143357b2b9404
YoussefMassoud <youssefbusiness133@gmail.com>
2024-03-12 14:45:17 +0200

    MOSearch bar make it done

--- app/(components)/Pages/Home.tsx
+++ app/(components)/Pages/Home.tsx
@@ -2,13 +2,16 @@ import NameOfSite from "../NameOfSite";
 import MainCard from "../MainCard";
 import FavouriteCard from "../FavouriteCard";
 import Image from "next/image";
-import DeviceFilter from "@/app/DeviceFilter";
+import Msearch from "../M-Search-bar";
+// import DeviceFilter from "@/app/DeviceFilter";
 
 export default function Home() {
   return (
     <>
       <NameOfSite />
-      <div className="w-full flex justify-center">
+      <div className="mt-5"> <Msearch  /></div>
+     
+      <div className="w-full flex justify-center mt-10">
         <div className=" shadow-lg rounded-xl p-6 lg:p-12 bg-[#1A004A80] ">
           <div className="flex mb-3 flex-row gap-6 justify-between ">
             <div>
@@ -19,9 +22,9 @@ export default function Home() {
                 PICK YOUR GAME
               </h3>
             </div>
-            <div className="font-bold mt-6 h-[26px] rounded-lg  w-fit py-1 px-6">
+            {/* <div className="font-bold mt-6 h-[26px] rounded-lg  w-fit py-1 px-6">
              <DeviceFilter />
-            </div>
+            </div> */}
           </div>
           <div className="grid lg:grid-rows-2  lg:grid-cols-2  justify-center gap-8">
             <FavouriteCard />
@@ -30,6 +33,7 @@ export default function Home() {
             <FavouriteCard />
           </div>
         </div>
+        
       </div>
     </>
   );
