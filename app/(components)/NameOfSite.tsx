5db1752261d15f816f8b4f069eceef0621cbd3d5
YasserAbdalla <yasserabdalla149@gmail.com>
2024-03-12 16:58:46 +0200

    Make the slug centered

--- app/(components)/NameOfSite.tsx
+++ app/(components)/NameOfSite.tsx
@@ -2,14 +2,13 @@
 
 export default function NameOfSite() {
   return (
-    <div className="flex justify-center items-center">
-      <div className="flex-row justify-center py-5  text-white text-5xl font-bold ">
-       Adhopemedia
-      <div className="flex-row justify-center text-white text-xl font-bold mt-2 ml-16  ">
-        Lorem Ipsum Text
+    <div className="flex flex-col justify-center items-center">
+      <div className="py-5  text-white text-5xl font-bold ">
+        Adhopemedia
+        <div className=" text-white text-xl font-bold mt-2 ml-16  ">
+          Lorem Ipsum Text
+        </div>
       </div>
-       </div>
-     
     </div>
   );
 }
