3b02ca1cf33b5777080adcede335ea185dd673d7
YoussefMassoud <youssefbusiness133@gmail.com>
2024-03-13 16:18:35 +0200

    fixed LgSearch BAr

--- app/(components)/LgSearchBar.tsx
+++ app/(components)/LgSearchBar.tsx
@@ -10,12 +10,16 @@ export default function LgSearchBar() {
           <FaSearch />
         </span>
         <input
-          type="search"
+          type="text"
           className="relative m-0 block w-full rounded-xl border-2 border-solid border-neutral-300 bg-white bg-clip-padding px-10 py-[0.25rem] text-base font-normal leading-[1.6] text-gray-950 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_#B064FE] focus:outline-none"
           placeholder="Search"
           aria-label="Search"
           aria-describedby="button-addon3 "
         />
+        <button
+          type="button"
+          className="absolute inset-y-0 end-0 flex items-center pe-3"
+        ></button>
       </div>
     </div>
   );
