a8469ee68e628ace6f2b8ea241549ce64f8cbdcd
YoussefMassoud <youssefbusiness133@gmail.com>
2024-03-15 00:17:11 +0200

    edit in button search

--- app/(components)/LgSearchBar.tsx
+++ app/(components)/LgSearchBar.tsx
@@ -10,18 +10,22 @@ const DynamicFaSearch = dynamic(
 
 export default function MSearchBar() {
   return (
-
-<form className=" mx-auto max-w-[160px] min-w-[160px] max-h-[15px] min-h-[15px] hidden lg:block">   
-    <label htmlFor="default-search" className="mb-0  text-sm font-medium text-gray-900 sr-only ">Search</label>
+<form className="max-w-[150px] min-w-[150px] hidden lg:block">   
+    <label htmlFor="default-search" className="mb-0 text-sm font-medium text-gray-900 sr-only">Search</label>
     <div className="relative z-1000">
         <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg className="w-4 h-4 text-[#B064FE] " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-            </svg>
+            <DynamicFaSearch className="w-4 h-4 text-[#B064FE]" />
         </div>
-        <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border-2 border-[#B064FE] rounded-3xl bg-gray-50 focus:ring-[#B064FE] focus:border-[#B064FE] " placeholder="Search Title..." required />
+        <input 
+            type="search" 
+            id="default-search" 
+            className="block w-full p-2 ps-10 text-sm text-gray-900 border-2 border-[#B064FE] rounded-3xl bg-gray-50 focus:ring-[#B064FE] focus:border-[#B064FE]" 
+            placeholder="Search Title..." 
+            required 
+        />
     </div>
 </form>
+
 
 
   );
