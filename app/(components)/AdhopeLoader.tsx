eada76124c6b2966f14bf6ab87fc105b3c870934
YasserAbdalla <yasserabdalla149@gmail.com>
2024-03-16 17:13:59 +0200

    Change the loader name

---
+++ app/(components)/AdhopeLoader.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+const AdhopeLoader = () => {
+  return (
+    <div className="preloader-container">
+  <div className="spinner-container">
+  <div className="spinner">
+   <div className="spinner">
+    <div className="spinner">
+      <div className="spinner">
+        <div className="spinner">
+            <div className="spinner"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+  );
+};
+
+export default AdhopeLoader;
